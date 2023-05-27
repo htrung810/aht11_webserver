5025ab327d8e4e7c22642ee0258cc8c199d4eb20
trungdz <htrung810@gmail.com>
2023-05-27 13:42:31 +0700

    fix

--- app.py
+++ app.py
@@ -79,4 +79,4 @@ async def get_metrics(request: Request, sensor: str, storage: aioredis.Redis = D
 
 
 if __name__ == "__main__":
-    uvicorn.run(app, host="0.0.0.0", port=int(os.getenv("PORT", 8000)))
+    uvicorn.run("app:app", host="0.0.0.0", port=int(os.getenv("PORT", 8000)))
